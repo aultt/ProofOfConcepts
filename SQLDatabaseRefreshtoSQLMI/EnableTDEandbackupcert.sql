671ff96d6bbc76b8bcf69e904e478bb7bd405701
Troy Ault <aultt@users.noreply.github.com>
2020-01-19 11:08:30 -0500

    adding comments to sql script

--- SQLDatabaseRefreshtoSQLMI/EnableTDEandbackupcert.sql
+++ SQLDatabaseRefreshtoSQLMI/EnableTDEandbackupcert.sql
@@ -1,17 +1,25 @@
+
+
 USE master;  
 GO  
 
-
+--Create database master key for and provide a password.  Replace the password below with one of your own.
 CREATE MASTER KEY ENCRYPTION BY PASSWORD = 'password';  
 go  
+
+--Create Certificate which will encrypt the database encryption key
 CREATE CERTIFICATE MyServerCert2 WITH SUBJECT = 'My DEK Certificate';  
 go  
 USE myTDETest2 ;  
 GO  
+
+--Create Database encryption and encrypt it with certificate we created
 CREATE DATABASE ENCRYPTION KEY  
 WITH ALGORITHM = AES_128  
 ENCRYPTION BY SERVER CERTIFICATE MyServerCert2;  
 GO  
+
+--Enable Encryption on the database
 ALTER DATABASE myTDETest2 
 SET ENCRYPTION ON;  
 GO
@@ -19,6 +27,8 @@ GO
 
 USE master
 GO
+
+--Verify Database is encrypted
 SELECT db.name as [database_name], cer.name as [certificate_name]
 FROM sys.dm_database_encryption_keys dek
 LEFT JOIN sys.certificates cer
@@ -29,6 +39,7 @@ WHERE dek.encryption_state = 3
 
 USE master
 GO
+--Backup Certificate.
 BACKUP CERTIFICATE MyServerCert2
 TO FILE = 'MyServerCert2.cer'
 WITH PRIVATE KEY (
