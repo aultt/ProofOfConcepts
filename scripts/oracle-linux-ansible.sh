3c2d62865884bc9abc3d07b3beefe99bbb60c1b3
Troy Ault <aultt@users.noreply.github.com>
2021-07-01 13:12:04 -0400

    Update oracle-linux-ansible.sh

--- scripts/oracle-linux-ansible.sh
+++ scripts/oracle-linux-ansible.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+while getopts g:o:r:w:u:y:s:m:d: option
+    do case "${option}"
+        in
+    g) GRIDPASS=${OPTARG};;
+    o) ORACLEPASS=${OPTARG};;
+    r) ROOTPASS=${OPTARG};;
+    w) SWAPSIZE=${OPTARG};;
+    u) STORAGEURL=${OPTARG};;
+    y) SYSPASS=${OPTARG};;
+    s) SYSTEMPASS=${OPTARG};;
+    m) MONITORPASS=${OPTARG};;
+    d) DBNAME=${OPTARG};;
+        esac
+        done
 # Update all packages that have available updates.
 sudo yum update -y
 
@@ -20,3 +34,27 @@ wget https://raw.githubusercontent.com/ansible-collections/azure/dev/requirement
 
 # Install Ansible modules
 sudo pip3 install -r requirements-azure.txt
+
+ansible-playbook Configure-ASM-server.yml -e gridpass=$GRIDPASS -e oraclepass=$ORACLEPASS -e rootpass=$ROOTPASS -e swapsize=$SWAPSIZE -e gridurl=$gridurl -e syspass=$SYSPASS -e systempass=$SYSTEMPASS -e monitorpass=$MONITORPASS -e dbname=$DBNAME
+
+## Update all packages that have available updates.
+#sudo yum update -y
+#
+## Install Python 3 and pip.
+#sudo yum install -y python3-pip
+#
+## Upgrade pip3.
+#sudo pip3 install --upgrade pip
+#
+## Install Ansible.
+#pip3 install ansible[azure]
+#
+## Install Ansible modules and plugins for interacting with Azure.
+#ansible-galaxy collection install azure.azcollection
+#
+## Install required modules for Ansible on Azure
+#wget https://raw.githubusercontent.com/ansible-collections/azure/dev/requirements-azure.txt
+#
+## Install Ansible modules
+#sudo pip3 install -r requirements-azure.txt
+#
